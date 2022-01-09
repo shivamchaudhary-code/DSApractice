b33313c681c34bf08b5d549c4f9769849daa8a7e
shivamchaudhary-code <shivamchaudhary177@gmail.com>
2022-01-10 02:33:24 +0530

    dev branch change 1

--- Array/find_the_max_and_min_element_from_array.cpp
+++ Array/find_the_max_and_min_element_from_array.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 	cout << "find the min and max element from the array" << endl;
-	cout << "solve the question";
+	cout << "solve the question" << endl;;
+	cout << "enter in the dev branch" << endl;
 	return ;
 }
