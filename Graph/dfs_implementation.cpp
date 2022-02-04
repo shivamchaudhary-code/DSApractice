e95dcb4e0d4d81833d53fe4dd5a2dba0d77e7ff0
shivamchaudhary-code <shivamchaudhary177@gmail.com>
2022-02-04 16:18:33 +0530

    new question added

---
+++ Graph/dfs_implementation.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<map>
+#include<list>
+#include<queue>
+using namespace std;
+
+template<typename T>
+class Graph {
+	map<T, list<T>> l;
+
+public:
+	void addEdge(int x, int y) {
+		l[x].push_back(y);
+		l[y].push_back(x);
+	}
+
+	void dfs_helper(T src, map<T, bool> &visited) {
+		cout << src << " ";
+		visited[src] = true;
+// go to all nbr of node that is not visited
+		for (T nbr : l[src]) {
+			if (!visited[nbr]) {
+				dfs_helper(nbr, visited);
+			}
+		}
+	}
+
+	void dfs(T src) {
+		map<T, bool> visited;
+//mark all nodes as not visited in the begning
+		for (auto p : l) {
+			T node = p.first;
+			visited[node] = false;
+		}
+		dfs_helper(src, visited);
+
+	}
+};
+
+//Main
+int main() {
+	Graph<int> g;
+	g.addEdge(0, 1);
+	g.addEdge(1, 2);
+	g.addEdge(2, 3);
+	g.addEdge(3, 4);
+	g.addEdge(4, 5);
+
+	g.dfs(0);
+}
