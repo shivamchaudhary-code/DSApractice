c78a0ab9e6d7359e517e859e92771baa0540a5c5
shivamchaudhary-code <shivamchaudhary177@gmail.com>
2022-02-10 17:43:24 +0530

    new code added

--- Array/find_the_max_and_min_element_from_array.cpp
+++ Array/find_the_max_and_min_element_from_array.cpp
@@ -1,10 +1,45 @@
-#include<iostream>
+// { Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
 
 int main() {
-	cout << "find the min and max element from the array" << endl;
-	cout << "solve the question" << endl;;
-	cout << "enter in the dev branch" << endl;
-	cout << "new change in dev branch" << endl;
-	return ;
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		ll a[n];
+		for (int i = 0; i < n; i++) cin >> a[i];
+
+		pair<ll, ll> pp = getMinMax(a, n);
+
+		cout << pp.first << " " << pp.second << endl;
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+	pair<long long, long long> p;
+	if (a[0] > a[1]) {
+		p.first = a[1];
+		p.second = a[0];
+	}
+	else {
+		p.first = a[0];
+		p.second = a[1];
+	}
+
+	for (int i = 2; i < n; i++) {
+		if (p.first > a[i]) {
+			p.first = a[i];
+		}
+		if (p.second < a[i]) {
+			p.second = a[i];
+		}
+	}
+	return p;
 }
